d101a4af3dff834f570230afcadc26a57fc659ee
Eric Fontana <eric.fontana@indico.io>
2022-12-20 08:34:35 -0500

    Install sealed secret

--- encrypt.sh
+++ encrypt.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-kubeseal --fetch-cert --controller-name mgl-sealed-secrets --controller-namespace mgl > mgl-sealed-cert.pem
-kubeseal < secrets.yaml --namespace mgl --cert mgl-sealed-cert.pem -o yaml > ./charts/calendar/templates/sealedsecrets.yaml
+kubeseal --fetch-cert  > mgl-sealed-cert.pem
+kubeseal < secrets.yaml --scope namespace-wide --namespace mgl --cert mgl-sealed-cert.pem -o yaml > ./charts/calendar/templates/sealedsecrets.yaml
